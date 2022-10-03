d471da46e2d4a40bf98b333e79db13916ab79fc5
mYusufHerlambang <muhammad.herlambang@indocyber.id>
2022-10-03 11:19:58 +0700

    add new file (ApplicationUserDetails), override method loadUserByUsername in AccountServiceImpl

--- src/main/java/com/adaLaundry/service/AccountServiceImpl.java
+++ src/main/java/com/adaLaundry/service/AccountServiceImpl.java
@@ -1,7 +1,33 @@
 package com.adaLaundry.service;
 
+import com.adaLaundry.ApplicationUserDetails;
+import com.adaLaundry.entity.Account;
+import com.adaLaundry.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService, UserDetailsService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Account> accountOptional = accountRepository.findById(username);
+
+        Account tempAccount = null;
+
+        if (accountOptional.isPresent()){
+            tempAccount = accountOptional.get();
+        }
+
+        return new ApplicationUserDetails(tempAccount);
+    }
 }
